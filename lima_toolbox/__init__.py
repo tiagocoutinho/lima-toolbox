3b30cd459d48cdf5d0c862c80bc939d775d3114f
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-05-16 19:50:45 +0200

    Bump version: 0.0.0 → 0.0.1

--- lima_toolbox/__init__.py
+++ lima_toolbox/__init__.py
@@ -1,3 +1,3 @@
 from .cli import main  # noqa
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     keywords="Lima, CLI, toolbox, detector",
     packages=find_packages(),
     url="https://github.com/tiagocoutinho/lima-toolbox",
-    version="0.0.0",
+    version="0.0.1",
     python_requires=">=3.5"
 )
