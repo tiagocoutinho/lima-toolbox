25cbd3a73b3c3f999c636c4aa371172c61b957a6
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-10-02 16:31:25 +0200

    setup: Add missing dependency on beautifultable

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 with open("README.md") as f:
     description = f.read()
 
-install_requires = ["click", "pint", "netifaces"]
+install_requires = ["click", "pint", "netifaces", "beautifultable"]
 
 extras_require = {
     "basler": ["pylonctl"],
